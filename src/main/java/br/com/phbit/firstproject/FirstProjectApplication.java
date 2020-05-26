99254eaadfe241c1fa29e708ebe7a7e99d394370
Marlon Dauernheimer <marlon@phbit.com.br>
2020-05-26 15:21:27 -0300

    change project to use postgress

--- src/main/java/br/com/phbit/firstproject/FirstProjectApplication.java
+++ src/main/java/br/com/phbit/firstproject/FirstProjectApplication.java
@@ -2,10 +2,14 @@ package br.com.phbit.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.com.phbit.firstproject.model"})
+@EnableJpaRepositories(basePackages = {"br.com.phbit.firstproject.repository"})
 public class FirstProjectApplication {
 	
 	@Bean
