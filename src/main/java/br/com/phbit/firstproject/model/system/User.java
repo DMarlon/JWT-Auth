d77931c5638da0bf1250af82fff0262454700646
Marlon Dauernheimer <marlon@phbit.com.br>
2020-06-03 10:36:50 -0300

    criado perfil do usuario

--- src/main/java/br/com/phbit/firstproject/model/system/User.java
+++ src/main/java/br/com/phbit/firstproject/model/system/User.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import br.com.phbit.firstproject.enumerators.Status;
@@ -17,16 +19,20 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "use_id")
+	@Column(name = "use_id", nullable = false)
 	private long id;
 	
-	@Column(name = "use_name")
+	@Column(name = "use_name", nullable = false)
 	private String name;
 	
-	@Column(name = "use_password")
+	@Column(name = "use_password", nullable = false)
 	private String password;
 	
-	@Column(name = "use_status")
+	@ManyToOne
+	@JoinColumn(name = "use_profile_id", nullable = false)
+	private Profile profile;
+	
+	@Column(name = "use_status", nullable = false)
 	@Convert(converter = StatusConvert.class)
 	private Status status;
 	
@@ -53,6 +59,15 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public Profile getProfile() {
+		return profile;
+	}
+	
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
 	
 	public Status getStatus() {
 		return status;
